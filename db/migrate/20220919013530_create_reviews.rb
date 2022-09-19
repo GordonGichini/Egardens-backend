8adcdd6e451236ffa87aa3c27ca3bb4bcca9ffc9
GordonGichini <gordongichini1@gmail.com>
2022-09-19 05:00:55 +0300

    created reviews migration

--- db/migrate/20220919013530_create_reviews.rb
+++ db/migrate/20220919013530_create_reviews.rb
@@ -1,4 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[7.0]
   def change
+    create_table :reviews do |t|
+      t.integer :product_id
+      t.integer :buyer_id
+      t.string :content
+    end
   end
 end
