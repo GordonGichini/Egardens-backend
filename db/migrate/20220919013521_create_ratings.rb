49d5235cd9def77f4824ba1290b8e4d171c47680
GordonGichini <gordongichini1@gmail.com>
2022-09-19 05:02:07 +0300

    created ratings migration

--- db/migrate/20220919013521_create_ratings.rb
+++ db/migrate/20220919013521_create_ratings.rb
@@ -1,4 +1,9 @@
 class CreateRatings < ActiveRecord::Migration[7.0]
   def change
+    create_table :ratings do |t|
+      t.integer :product_id
+      t.integer :buyer_id
+      t.integer :rating
+    end
   end
 end
