d7dd24fbfa23a5096a04472e8c5f3f193a41508a
GordonGichini <gordongichini1@gmail.com>
2022-09-19 05:03:12 +0300

    purchase migration

--- db/migrate/20220919013500__purchases.rb
+++ db/migrate/20220919013500__purchases.rb
@@ -1,4 +1,12 @@
 class Purchases < ActiveRecord::Migration[7.0]
   def change
+    create_table :purchases do |t|
+      t.integer :buyer_id
+      t.integer :product_id
+      t.integer :quantity
+      t.integer :cash_paid
+      t.string :status
+      t.timestamps
+    end
   end
 end
